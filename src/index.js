851d313849b3958e36f1de48b4d06410a11fec08
Zoltan Mics <zoltan@merez.merez>
2021-04-20 00:37:07 +0200

    added zsoltar 10 tonus

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Tonus from './ui/tonus';
 import ColorProvider from './colorProvider';
 import SingleElement from './ui/singleElement';
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let colorProvider = new ColorProvider();
 const tonusok = numbers.map(function (number, i) {
     return <Tonus counter={number} colourClass={colorProvider.getColour()} />
